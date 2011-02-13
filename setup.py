ead5ad42602aa85a910d06f9c8c02914321e90ea
John <John@Junction-Tree>
2011-02-13 15:25:28 -0800

    initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Wormhole',
+      version='0.1',
+      description='Use python from matlab and matlab from python',
+      author='John Schulman',
+      author_email='john.d.schulman@gmail.com',
+      url='http://',
+      py_modules=['Wormhole'],
+      scripts=['open_wormhole.py'],
+     )
